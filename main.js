6569fc3d50b6c133944f436fc2626481d4e8bb21
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-06-13 19:17:26 +0500

    Day 36

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+//             🚀 Day 36 Challenge: Start Coding! 🚀
+Object.defineProperty(exports, "__esModule", { value: true });
+// Question 106: 
+// Determine if a given year is a leap year using comparison operators.
+function isLeapYearOrNot(year) {
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0; // checks if the year is a leap year or not
+}
+console.log(isLeapYearOrNot(2020)); // Outputs: true
+console.log(isLeapYearOrNot(2021)); // Outputs: false
+// Question 107: 
+// Write a condition using logical operators that checks if a number is divisible by both 2 and 3.
+function isDivisibleByTwoAndThree(number) {
+    return number % 2 == 0 && number % 3 == 0;
+}
+console.log(isDivisibleByTwoAndThree(12)); // Outputs: true
+console.log(isDivisibleByTwoAndThree(16)); // Outputs: false
+// Question 108:
+// Compare two strings to check if they are identical, ignoring case sensitivity.
+function compareStrings(string1, string2) {
+    return string1.toLowerCase() == string2.toLowerCase();
+}
+console.log(compareStrings("hello", "Hello")); // output is true
